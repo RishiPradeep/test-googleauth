9ab658bb27d03ffde2e342cc5141d745da4fdfdb
Rishi Pradeep <rishipradeep@gmail.com>
2024-08-15 22:02:51 +0530

    removed keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD13JZ8UlFkPFOFQ4LIh2_mwySJ6ugZEuQ",
-  authDomain: "fir-auth-a31ab.firebaseapp.com",
-  projectId: "fir-auth-a31ab",
-  storageBucket: "fir-auth-a31ab.appspot.com",
-  messagingSenderId: "930748767164",
-  appId: "1:930748767164:web:41cbb056a01e4ab16ee9b9",
-  measurementId: "G-0LM18NRX11",
+  //Firebase config here
 };
 
 const app = initializeApp(firebaseConfig);
